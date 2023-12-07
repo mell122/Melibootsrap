a27cb7e8da9c808d3e9a21ccdd88b739d7073259
mell122 <94059072+mell122@users.noreply.github.com>
2023-12-07 12:14:06 -0500

    api

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 
-def test():
-    print("hello")
 
-@app.get("/")
-def root():
-    return("hello")
+@app.get("/{number}")
+def divisibality(number):
 
-@app.get("/login")
-def login():
-    return("login")
+    variable = int(number) / 2
+
+    if variable.is_integer() :
+        return("even")
+    else:
+        return("odd")
+    
+
 
 if __name__ == "__main__" :
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("Homework:app", host= "0.0.0.0", port= 5001, reload=True)
